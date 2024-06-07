b51abd64f87928dc9997ca86283aa670c793ac05
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-06-07 14:33:59 +0700

    tut1

---
+++ tut1/web1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using web1.Models;
+
+namespace web1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //URL: localhost:PORT/Home/Demo
+        public IActionResult Demo()
+        {
+            //Path: Views/Home/Demo.cshtml
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
